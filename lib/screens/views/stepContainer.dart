859b77a4ad6c8a84c3295b229303585e26db6851
Malte Bünz <maltebuenz@posteo.de>
2021-02-08 20:59:04 +0100

    add widget top row

--- lib/screens/views/stepContainer.dart
+++ lib/screens/views/stepContainer.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wegli/screens/components/decortations.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
 
 class StepContainer extends StatelessWidget {
-  final Widget child;
+  StepContainer(
+      {Key key,
+      this.child,
+      this.isComplete = false,
+      this.isExpanded = false,
+      this.title = 'Title'})
+      : super(key: key);
 
-  const StepContainer({Key key, this.child}) : super(key: key);
+  final Widget child;
+  bool isComplete;
+  final bool isExpanded;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +24,35 @@ class StepContainer extends StatelessWidget {
       decoration: CustomDecorations().baseBackgroundDecoration(),
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.all(20),
-      child: child,
-      );
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(
+                  isComplete
+                      ? CupertinoIcons.exclamationmark_circle_fill
+                      : CupertinoIcons.checkmark_circle_fill,
+                  color: isComplete ? Colors.green : Colors.orange,
+                  size: 24.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+              ),
+              Spacer(),
+              Icon(
+                isExpanded
+                    ? CupertinoIcons.chevron_down_circle
+                    : CupertinoIcons.chevron_up_circle,
+              )
+            ],
+          ),
+          child
+        ],
+      ),
+    );
   }
 }
